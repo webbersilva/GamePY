8c544fad74cf28ed1def3a6f252352e645de1fde
weber silva <webersilva555@gmail.com>
2024-06-06 09:24:01 -0300

    second commit

--- game.py
+++ game.py
@@ -5,7 +5,26 @@ def main() -> None:
     jogar(pontos)
 
 def jogar(pontos: int) -> None:
-    pass
+    dificuldade: int = int(input('Informe o nível de dificuldade desejado[1, 2, 3 ou 4]'))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print("Informe o resultadon para a seguinte operação")
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} pontos(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo(1 - sim, 0 -não'))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Você finalizou com {pontos} ponto(s).')
+        print('Até a proxima!')
 
 if __name__ == '__main__':
     main()
