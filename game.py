da623c7dcb2ea84423756874298418730d39fb7c
weber silva <webersilva555@gmail.com>
2024-06-06 09:13:54 -0300

    first commit

---
+++ game.py
@@ -0,0 +1,11 @@
+from models.calcular import Calcular
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+def jogar(pontos: int) -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
