d00241690fe96743bcd4dc061c23a8dabd668ed5
weber silva <webersilva555@gmail.com>
2024-06-06 09:53:50 -0300

    third commit

--- models/calcular.py
+++ models/calcular.py
@@ -6,7 +6,7 @@ class Calcular:
         self.__valor1: int = self._gerar_valor
         self.__valor2: int = self._gerar_valor
         self.__operacao: int = randint(1, 3) # 1 = somar, 2 = diminuir, 3 = multiplicar
-        self.__resultado: int + self._gerar_resultado
+        self.__resultado: int = self._gerar_resultado
 
     @property
     def dificuldade(self: object) -> int:
@@ -42,14 +42,45 @@ class Calcular:
 
     @property
     def _gerar_valor(self: object) -> int:
-        pass
+        if self.dificuldade == 1:
+            return randint(0, 10)
+        elif self.dificuldade == 2:
+            return randint(0, 100)
+        elif self.dificuldade == 3:
+            return randint(0, 1000)
+        elif self.dificuldade == 4:
+            return randint(0, 10000)
+        else:
+            return randint(0, 100000)
 
     @property
-    def _gerar_resulatdo(self: object) -> int:
-        pass
+    def _gerar_resultado(self: object) -> int:
+        if self.operacao == 1: #somar
+            return self.valor1 + self.valor2
+        elif self.operacao == 2: #diminuir
+            return self.valor1 - self.valor2
+        else:
+            return self.valor1 + self.valor2
+
+    @property
+    def _op_simbolo(self: object) -> str:
+        if self.operacao == 1:
+            return '+'
+        elif self.operacao == 2:
+            return '-'
+        else:
+            return'+'
 
     def checar_resultado(self: object, resposta: int) -> bool:
-        pass
+        certo: bool = False
+
+        if self.resultado == resposta:
+            print('resposta correta!')
+            certo = True
+        else:
+            print("Resposta errada!")
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = {self.resultado}')
+        return certo
 
     def mostrar_operacao(self: object) -> None:
-        pass
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = ?')
